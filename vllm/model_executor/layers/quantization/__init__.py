f80969283b826e7c89924ebb18cc136fb0b9782d
lyg95 <li.yangguang@zte.com.cn>
2024-04-22 09:27:03 +0800

    Support autoquant in vllm-v0.4.1 (#7)
    Signed-off-by: lyg95 <li.yangguang@zte.com.cn>

--- vllm/model_executor/layers/quantization/__init__.py
+++ vllm/model_executor/layers/quantization/__init__.py
@@ -6,12 +6,14 @@ from vllm.model_executor.layers.quantization.base_config import (
 from vllm.model_executor.layers.quantization.gptq import GPTQConfig
 from vllm.model_executor.layers.quantization.marlin import MarlinConfig
 from vllm.model_executor.layers.quantization.squeezellm import SqueezeLLMConfig
+from vllm.model_executor.layers.quantization.auto_quant import AutoQuantConfig
 
 QUANTIZATION_METHODS = {
     "awq": AWQConfig,
     "gptq": GPTQConfig,
     "squeezellm": SqueezeLLMConfig,
     "marlin": MarlinConfig,
+    "autoquant": AutoQuantConfig,
 }
 
 
